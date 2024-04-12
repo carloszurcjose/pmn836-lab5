e79475b926207cffb870c9c8de77c0dac56b40f2
Carlos Cruz <carlosjoecruz@gmail.com>
2024-04-12 01:00:16 -0500

    initial

---
+++ app/src/main/java/com/cruzurc/pmn836_lab5/MainActivity.java
@@ -0,0 +1,37 @@
+package com.cruzurc.pmn836_lab5;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.cruzurc.pmn836_lab5.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        /*
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        */
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/cruzurc/pmn836_lab5/model/Scene.java
@@ -0,0 +1,4 @@
+package com.cruzurc.pmn836_lab5.model;
+
+public class Scene {
+}
